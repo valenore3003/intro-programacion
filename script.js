475b348701e1fe68aba890226bf3d5ecce3a4e43
valenore3003 <valen.ore64@gmail.com>
2025-10-01 23:49:31 +0000

    fifth commit

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-const button = document.getElementById("submit")
-const input = document.getElementById("texto")
-button.onclick = (e) =>{
-    e.preventDefault()
-    console.log(input.value)
+const nombre= "valentina"
+let edad= 25
+const saludo= `Hola mi nombre es ${nombre}, tengo ${edad} años`
+console.log(nombre)
+console.log(edad)
+console.log(saludo)
+console.log("1"==1)
+console.log("1"===1)
+const lista=[1,"valentina"]
+lista.length
+console.log(lista)
+function saludar (nombre) {
+console.log(`Hola ${nombre}`)
+return `Hola ${nombre} retonrno`
 }
+saludar("Valentina")
+console.log(saludar("Valentina"))
+const misaludar=saludar("Valentina")
